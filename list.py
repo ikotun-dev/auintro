60551b08e257b1abf60a8b2c380e888c91ab7f49
Ikotun Collins <danlogan2003@gmail.com>
2023-08-08 10:11:40 +0100

    list intro

---
+++ list.py
@@ -0,0 +1,12 @@
+animals = ['dog', 'cat', 'horse', 'cow', 'sheep']
+new_animal_1 = str(input("Enter your fist animal : "))
+new_animal_2 = str(input("Enter your second animal : "))
+
+animals.append(new_animal_1)
+animals.append(new_animal_2)
+
+
+#print(animals[0])
+print(animals)
+
+#print(type(animals))
